ee14cd04557e0677a1913717f3ab655ccbb7bc55
sudhasew <sudha.sew@gmail.com>
2021-11-03 11:23:33 -0400

    added attackPlayer,logHealth

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const randomDamage = () => {
   let randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
   return randomNumber;
 };
-var chooseOption = (opt1, opt2) => {
+const chooseOption = (opt1, opt2) => {
   var randNum = Math.floor(Math.random() * 2);
   var optionSelected = randNum === 0 ? opt1 : opt2;
   return optionSelected;
@@ -12,3 +12,9 @@ function attackPlayer(health) {
   var result = health - randomDamage();
   return result;
 }
+const logHealth = (player, health) => {
+  console.log(`${player} health: ${health}`);
+};
+const logDeath = (winner, loser) => {
+  console.log(`${winner} defeated ${loser}`);
+};
