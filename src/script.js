0616174cc53303aa82c8792e7cecd79cfd7c6423
sudhasew <sudha.sew@gmail.com>
2021-11-06 21:01:49 -0400

    Final commit

--- src/script.js
+++ src/script.js
@@ -1,28 +1,25 @@
 "use strict";
-const randomDamage = () => {
-  let randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-  return randomNumber;
-};
+const randomDamage = () => Math.floor(Math.random() * 10 + 1);
+console.log(randomDamage());
+
 const chooseOption = (opt1, opt2) => {
-  var randNum = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-  var optionSelected = randNum === 0 ? opt1 : opt2;
-  return optionSelected;
+  const randNum = Math.round(Math.random());
+  return randNum === 0 ? opt1 : opt2;
 };
+
 function attackPlayer(health) {
   var result = health - randomDamage();
   return result;
 }
+
 const logHealth = (player, health) => {
   console.log(`${player} health: ${health}`);
 };
+
 const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
-const isDead = (health) => {
-  if (health <= 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+
+const isDead = (health) => health <= 0;
+
 function fight(player1, player2, player1Health, player2Health) {
   while (true) {
     var attacker = chooseOption(player1, player2);
@@ -43,4 +40,5 @@ function fight(player1, player2, player1Health, player2Health) {
     }
   }
 }
+
 fight("Sudha", "Chandu", 100, 100);
